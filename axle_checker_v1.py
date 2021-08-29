9b2eb0f6aa30704d1e11a091407320f222cb968e
robertlyttle32 <43766939+robertlyttle32@users.noreply.github.com>
2021-08-29 18:45:05 -0500

    Add files via upload

--- axle_checker_v1.py
+++ axle_checker_v1.py
@@ -10,35 +10,35 @@ class presentsChecker:
         self.stop_exit = stop_exit
 
     def presents_checker(self):
+        global start_entry
+        global stop_entry
+        global start_exit
+        global stop_exit
+
         entries = []
         entries = [self.start_entry,self.stop_entry,self.start_exit,self.stop_exit]
 
         #start_entry
-        if entries == [1,0,0,0] or [1,1,0,0]:
-            global start_entry
+        if entries == [1,0,0,0]:
             start_entry = 'start_entry_{}'.format(get_time())
             #print('start entry entries: ', entries)
             print(start_entry)
 
         #stop_entry
-        if entries == [0,1,0,0] or [0,1,1,0]:
-            global stop_entry
+        if entries == [0,1,0,0]:
             stop_entry = 'stop_entry_{}'.format(get_time())
             #print('stop entry entries: ', entries)
             print(stop_entry)
 
         #start_exit
-        if entries == [0,0,1,0] or [0,0,1,1]:
-            global start_exit
+        if entries == [0,0,1,0]:
             start_exit = 'start_exit_{}'.format(get_time())
             #print('start exit entries: ', entries)
             print(start_exit)
 
         #stop_exit
         if entries == [0,0,0,1]:
-            global stop_exit
             stop_exit = 'stop_exit_{}'.format(get_time())
-            #print('stop exit entries: ', entries)
             print(start_entry,stop_entry,start_exit,stop_exit)
             return start_entry,stop_entry,start_exit,stop_exit
 
