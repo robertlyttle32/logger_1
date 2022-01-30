566ed81f855a43f6742387b575aba6cb06012e63
robertlyttle32 <43766939+robertlyttle32@users.noreply.github.com>
2022-01-30 10:20:56 -0600

    Update rdl_auditor_dependencies.sh

--- rdl_auditor_dependencies.sh
+++ rdl_auditor_dependencies.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-
+sed -i -e 's/\r$//' auditor_dependencies.sh #run if script crashes
+#if tkcalendar crashes run
+sudo add-apt-repository ppa:j-4321-i/ppa
 sudo apt-get update
 sudo apt-get install python3-pip
 sudo pip3 install numpy
