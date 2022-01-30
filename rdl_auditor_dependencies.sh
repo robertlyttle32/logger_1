91de4f1ea13ce9952dbb4d17191080d39f60c2ea
robertlyttle32 <43766939+robertlyttle32@users.noreply.github.com>
2022-01-30 10:16:44 -0600

    Add files via upload

---
+++ rdl_auditor_dependencies.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install python3-pip
+sudo pip3 install numpy
+sudo pip3 install opencv-python
+sudo pip3 install rq
+sudo pip3 install tkcalendar
+sudo pip3 install pillow
+sudo pip3 install urllib3
+sudo apt-get install python3-pil
+sudo apt-get install python3-pil.imagetk
+sudo add-apt-repository ppa:j-4321-i/ppa
+#Install to resize image
+#sudo apt-get install imagemagick
+#convert my_logo.png -resize 200x200 my_logo1.png
